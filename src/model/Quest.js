20227ad567ac9c93d1908283cdbdfd24d68ded0c
차상원 <cktkddnjs1@icloud.com>
2022-06-15 01:00:30 +0900

    수정: sql의 흔적 제거

--- src/model/Quest.js
+++ src/model/Quest.js
@@ -20,20 +20,4 @@ useVirtualId(QuestSchema);
 
 const Quest = mongoose.model("Quest", QuestSchema);
 
-// const Quest = sqHelper.define("quest", {
-//   id: {
-//     type: SQ_Datatypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   title: {
-//     type: SQ_Datatypes.STRING(40),
-//     allowNull: false,
-//   },
-//   created_user_id: {
-//     type: SQ_Datatypes.INTEGER,
-//     allowNull: false,
-//   },
-// });
-
 export default Quest;
